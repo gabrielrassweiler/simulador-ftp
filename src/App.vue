9bd718fb33548127ecd1255a0821c6988b13a06e
Gabriel <gabriel@conectra.com.br>
2022-03-24 21:59:02 -0300

    feat(usuario,requisição): form do usuario com os tratamentos e login, requisicao com o envio do arquivo para o servidor

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,35 @@
     <div class="row">
       <h1 class="d-flex justify-content-center mt-3 fs-7">Simulador FTP</h1>
 
-      <fieldset class="border p-3 rounded-3 mb-3">
-        <legend class="float-none fs-6 w-auto">Local</legend>
+      <fieldset class="border p-3 rounded-3 mb-3 shadow">
+        <legend class="float-none fs-6 w-auto">Usuário</legend>
+
+        <div class="row g-2">
+          <div class="col-md-3">
+            <label class="form-label">Host</label>
+            <input type="text" id="host" class="form-control"/>
+          </div>
+          <div class="col-md-3">
+            <label class="form-label">Usuário</label>
+            <input type="text" id="usuario" class="form-control"/>
+          </div>
+          <div class="col-md-3">
+            <label class="form-label">Senha</label>
+            <input type="password" id="senha" class="form-control"/>
+          </div>
+          <div class="col-md-3">
+            <button class="btn btn-primary btnUsuario" @click="this.conectaUsuario()">Enviar</button>
+          </div>
+        </div>
+      </fieldset>
+
+      <fieldset class="border p-3 rounded-3 mb-3 shadow">
+        <legend class="float-none fs-6 w-auto">Cliente</legend>
 
         <form class="row g-2">
           <div class="col-md-6">
             <label class="mb-3">Escolhe um arquivo para envio:</label><br>
-            <input type="file" class="form-control-file" @change="enviarArquivo">
+            <input type="file" class="form-control-file" @change="validaArquivo">
           </div>
           <div class="col-md-6">
             <label class="form-label">Informações arquivo</label>
@@ -18,7 +40,7 @@
         </form>
       </fieldset>
 
-      <fieldset class="border p-3 rounded-3 mb-3">
+      <fieldset class="border p-3 rounded-3 mb-3 shadow">
         <legend class="float-none fs-6 w-auto">Servidor</legend>
 
         <form class="row g-2">
@@ -30,11 +52,11 @@
 
           <div class="col-md-6">
             <label class="form-label">Respostas</label>
-            <textarea class="form-control" id="textAreaServidor" rows="7" disabled></textarea>
+            <textarea class="form-control" id="respostaServidor" rows="7" disabled></textarea>
           </div>
           <div class="col-md-6">
             <label class="form-label">Arquivos</label>
-            <textarea class="form-control" id="textAreaArquivosServidor" rows="7" disabled></textarea>
+            <textarea class="form-control" id="arquivosServidor" rows="7" disabled></textarea>
           </div>
         </form>
       </fieldset>
@@ -49,19 +71,26 @@ export default {
   data() {
     return {
       statusServidor: 'Desligado',
-      infoArquivoLocal: ''
+      infoArquivoLocal: '',
+      logado: false,
     }
   },
   methods: {
-    enviarArquivo(event) {
+    validaArquivo(event) {
       const file = event.target.files[0];
 
       if (this.statusServidor === 'Ligado') {
-        const tamanho = this.calculaTamanhoArquivo(file);
+        if (this.logado) {
+          const tamanho = this.calculaTamanhoArquivo(file);
 
-        this.infoArquivoLocal = 'Nome: ' + file.name + '\nTamanho : ' + tamanho + '\nTipo: ' + file.type;
+          this.infoArquivoLocal = 'Nome: ' + file.name + '\nTamanho: ' + tamanho + '\nTipo: ' + file.type;
+          
+          this.enviarArquivo(file);
+        } else {
+          alert('Usuário não logado! Favor conectar-se!');
+        }
       } else {
-        alert('Servidor está desligado! Favor ligar o mesmo para continuar com as operações')
+        alert('Servidor está desligado! Favor ligar o mesmo para continuar com as operações');
       }
     },
     calculaTamanhoArquivo(file) {
@@ -83,7 +112,64 @@ export default {
         tamanho = tamanho + "kb";
       }
       return tamanho;
-    }
+    },
+    conectaUsuario() {
+      this.requestServidor('usuario', document.getElementById('usuario').value);
+
+      if (this.requestServidor('senha', document.getElementById('senha').value)) {
+        this.logado = true;
+        this.requestServidor('host', document.getElementById('host').value);
+      }
+    },
+    requestServidor(input, elemento) {
+      let resultado;
+
+      if (this.statusServidor === 'Ligado') {
+        switch (input) {
+          case "usuario":
+            resultado = "220 Serviço pronto para novo usuário. \n";
+            if (elemento !== null) {
+              resultado = "331 Nome de usuário ok, precisa de senha. \n";
+            } else {
+              resultado = "332 Precisa de conta para login. \n";
+            }
+            break;
+          case "senha":
+            if (elemento === 'a') {
+              resultado = "230 Usuário logado. \n";
+              return true;
+            }
+
+            resultado = "430 Senha inválida. \n";
+            break;
+          case "host":
+            resultado = "200 A ação solicitada foi concluída com sucesso. \n";
+            break;
+          case "envio":
+            resultado = elemento === 'aberto'
+              ? "150 Status do arquivo ok, aberta a conexão de dados. \n"
+              : "226 Fechando a conexão de dados. Ação de arquivo solicitada bem-sucedida. \n";
+            break;
+          default:
+            resultado = "502 Comando não implementado. \n";
+            break;
+        }
+      } else {
+        resultado = "10060 Não foi possível se conectar com o servidor. \n";
+      }
+
+      document.getElementById("respostaServidor").value += resultado;
+    },
+    enviarArquivo(file) {
+      if (this.statusServidor === 'Ligado') {
+        this.requestServidor('envio', 'aberto');
+        document.getElementById("arquivosServidor").value += file.name + '\n';
+
+        this.requestServidor('envio', 'sucesso');
+      } else {
+        this.requestServidor('', '');
+      }
+    },
   }
 }
 </script>
@@ -92,5 +178,14 @@ export default {
 body {
   background-color: #f9f9f9;
   font-family: sans-serif;
+}
+
+.btnUsuario {
+  margin-top: 30px;
+  float: right;
+}
+
+h1 {
+  text-shadow: 3px 2px 3px #999999;
 }
 </style>
